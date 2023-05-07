5b6d56cf175b150dd0eca94bca37bd00685b5a47
Hidden <imboden.samuel@protonmail.ch>
2023-05-07 21:20:21 +0200

    updatre mongo

--- ui/src/ITask.ts
+++ ui/src/ITask.ts
@@ -1,4 +1,6 @@
-export interface TaskSchema {
+import moment from "moment";
+
+export class TaskSchema {
   name: string;
   description?: string;
   category?: string;
@@ -6,16 +8,70 @@ export interface TaskSchema {
   data?: DataEntry[];
   schedule: TaskSchedule;
   [k: string]: unknown;
+
+  constructor(task: TaskSchema) {
+    this.name = task.name;
+    this.description = task.description;
+    this.category = task.category;
+    this.priority = task.priority;
+    this.data = task.data;
+    this.schedule = task.schedule;
+  }
+
+  // Define a static method for creating a TaskSchema object from a plain JavaScript object
+  static fromDocument(doc: any): TaskSchema {
+    const task: TaskSchema = {
+      name: doc.name,
+      priority: doc.priority,
+      schedule: doc.schedule,
+    };
+
+    if (doc.description) {
+      task.description = doc.description;
+    }
+
+    if (doc.category) {
+      task.category = doc.category;
+    }
+
+    if (doc.data) {
+      task.data = doc.data;
+    }
+
+    // Convert the start_date and due_date fields to Date objects
+    if (doc.schedule.start_date) {
+      task.schedule.start_date = moment(doc.schedule.start_date);
+    }
+
+    if (doc.schedule.due_date) {
+      task.schedule.due_date = moment(doc.schedule.due_date);
+    }
+
+    if (doc.schedule.repeat_frequency) {
+      task.schedule.repeat_frequency = doc.schedule.repeat_frequency;
+    }
+
+    if (doc.schedule.repeat_days) {
+      task.schedule.repeat_days = parseInt(
+        doc.schedule.repeat_days.slice(2),
+        2
+      );
+    }
+
+    return task;
+  }
 }
-export interface DataEntry {
+
+export class DataEntry {
   typedef?: string;
   value?: string;
   [k: string]: unknown;
 }
-export interface TaskSchedule {
-  start_date?: string;
-  due_date?: string;
+
+export class TaskSchedule {
+  start_date?: moment.Moment;
+  due_date?: moment.Moment;
   repeat_frequency?: "daily" | "weekly" | "monthly";
-  repeat_days?: string;
+  repeat_days?: number;
   [k: string]: unknown;
 }
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,41 +1,43 @@
-import { app, BrowserWindow } from 'electron';
-
-import ejs from 'ejs';
+import { app, BrowserWindow } from "electron";
+import ejs from "ejs";
+import { readRandom } from "./MongoDB_API";
+import { TaskSchema, DataEntry, TaskSchedule } from "./ITask";
+import moment from "moment";
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
+    width: 422,
     height: 600,
-  })
-  let template = ejs.fileLoader('src/templates/main.ejs')
-  let html : string;
-  if(typeof template === 'string'){
-    html = ejs.render(template,{task});
-  }
-  else {
-    html = ejs.render(template.toString(),{task});
-  }
-  
-  win.loadURL("data:text/html;charset=utf-8," + encodeURI(html));
-}
+  });
+  readRandom().then((value) => {
+    if (value == null) return;
+    const task = TaskSchema.fromDocument(value);
+    task.duration = moment
+      .duration(task.schedule.due_date?.diff(task.schedule.start_date))
+      .asDays();
+    console.log(task);
+    let template = ejs.fileLoader("src/templates/main.ejs");
+    let html: string;
+    if (typeof template === "string") {
+      html = ejs.render(
+        template,
+        { task, moment },
+        { filename: "src/templates/main.ejs" }
+      );
+    } else {
+      html = ejs.render(
+        template.toString(),
+        { task, moment },
+        { filename: "src/templates/main.ejs" }
+      );
+    }
+    win.loadURL("data:text/html;charset=utf-8," + encodeURI(html));
+    win.addListener("resized", () => {
+      console.log(win.getSize());
+    });
+  });
+};
 
 app.whenReady().then(() => {
-  createWindow()
-})
-
-
-const task = {
-  "name": "Market Research Report",
-  "description": "Complete a report on market research for the new product launch",
-  "category": "Marketing",
-  "start_date": "2023-04-19T09:00:00Z",
-  "due_date": "2023-05-01T17:00:00Z",
-  "priority": 9,
-  "repeating": null,
-  "data": [
-    {
-      "data_type": "@type/market-research",
-      "data_value": { }
-    }
-  ]
-};
+  createWindow();
+});
