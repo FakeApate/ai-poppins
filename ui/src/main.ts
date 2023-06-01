0fb11805b50d978771cd4e88fc856c256e6e5728
Fake Apate <imboden.samuel@protonmail.ch>
2023-06-01 12:31:42 +0200

    remove mongo

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -9,43 +9,11 @@ const createWindow = () => {
     width: 422,
     height: 600,
   });
-  //#region mongodb
-  // //MongoDB
-  // readRandom().then((value) => {
-  //   if (value == null) return;
-  //   const task = TaskSchema.fromDocument(value);
-  //   task.duration = moment
-  //     .duration(task.schedule.due_date?.diff(task.schedule.start_date))
-  //     .asDays();
-  //   console.log(task);
-  //   let template = ejs.fileLoader("src/templates/main.ejs");
-  //   let html: string;
-  //   if (typeof template === "string") {
-  //     html = ejs.render(
-  //       template,
-  //       { task, moment },
-  //       { filename: "src/templates/main.ejs" }
-  //     );
-  //   } else {
-  //     html = ejs.render(
-  //       template.toString(),
-  //       { task, moment },
-  //       { filename: "src/templates/main.ejs" }
-  //     );
-  //   }
-  //   win.loadURL("data:text/html;charset=utf-8," + encodeURI(html));
-  //   win.addListener("resized", () => {
-  //     console.log(win.getSize());
-  //   });
-  // });
-  //#endregion
-  //#region file
-  //File
 
-  const dirpath = "mongoPlayground/";
+  const dirpath = "tasks/";
   let dir = fs.readdirSync(dirpath);
   dir = dir.filter((value: string) => {
-      return value.includes("json") ? (value.includes("aipoppings_2") ? null : value) : null;
+      return value.includes("json") ? value: null;
   });
   let filename = dir[Math.round(Math.random()* (dir.length-1))];
   const fileDoc = fs.readFileSync(dirpath + filename).toString();
